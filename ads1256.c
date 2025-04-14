5c16ff302404f220598cd086d94122208eab1c8b
rokkiea <i@rokkiea.dev>
2025-04-14 21:16:34 +0800

    feat: Fully implement the example program ads1256.c.

--- ads1256.c
+++ ads1256.c
@@ -51,8 +51,20 @@
 #define DEV_SPI_SPEED 1920000 // 1MHz
 #define DEV_SPI_BIT_P_WORD 8
 
+extern int ADS125xDriverDebug;
+char *usage = "Usage: [options...]"
+              " -h, --help                 Show this manual\n"
+              " -s, --single               Single read\n"
+              " -c, --continuous <times>   Read data 'times' times in continuous mode.\n"
+              "     -o, --output <file>    Write continuous mode data to a file\n"
+              " -p, --pdwn [off/on/0/1]    Set PDWN low (off/0) or high (on/1)\n\n"
+              "ads1256 homepage at: https://github.com/rokkiea/ADS125x-driver\n"
+              "Copyright (c) 2025 Guo Ruijing (rokkiea)";
+
 void one_shot_read();
 void continu_read(FILE *output, int times);
+void doContinuRead(int argc, char* argv []);
+void doPdwn(int argc, char* argv []);
 
 void one_shot_read()
 {
@@ -118,6 +130,7 @@ void one_shot_read()
     ads125xSetPDWN(&ads1256, 0);
     ads125xCloseDRDY(&ads1256);
     SPIRelease(ads1256.fd);
+    return;
 }
 
 void continu_read(FILE *output, int times)
@@ -195,11 +208,98 @@ void continu_read(FILE *output, int times)
     ads125xSetPDWN(&ads1256, 0);
     ads125xCloseDRDY(&ads1256);
     SPIRelease(ads1256.fd);
+    return;
 }
 
-int main(void)
+void doContinuRead(int argc, char* argv [])
 {
-    one_shot_read();
-    continu_read(stdout, 1000);
+    FILE *fp = NULL;
+    if (argc < 3) {
+        fprintf (stderr, "Usage: %s -c/--continuous <times>\n", argv [0]) ;
+        exit (1);
+    }
+    else if (argc == 4 || argc > 5)
+    {
+        fprintf (stderr, "Usage: %s -c/--continuous <times> -o <files>\n", argv[0]);
+        exit (1);
+    }
+
+    switch (argc)
+    {
+        case 5: 
+            if((fp = fopen(argv[4], "w")) == NULL)
+            {
+                fprintf(stderr, "Open file %s error.\n", argv[4]);
+                exit(EXIT_FAILURE);
+            }
+            continu_read(fp, atoi(argv[2])); break;
+        case 3:
+        default: continu_read(stdout, atoi(argv[2])); break;
+    }
+    return;
+}
+
+void doPdwn(int argc, char* argv [])
+{
+    int ret = 0;
+    ads125x_dev ads1256;
+
+    if (argc == 2 || argc > 3) {
+        fprintf (stderr, "Usage: %s -p/--pdwn [off/on/0/1]\n", argv [0]) ;
+        exit (1) ;
+    }
+
+    if ((ret = ads125xOpenPDWN(&ads1256, ADS125x_PDWN_CHIP, ADS125x_PDWN_LINE, 0)))
+        fprintf(stderr, "Open PDWN err: %d\n", ret);
+    // Set PDWN to high to POWER-UP ADS1256
+    
+    /**/ if ( strcasecmp(argv[2], "off") == 0 || strcasecmp(argv[2], "0") == 0 ) ads125xSetPDWN(&ads1256, 0);
+    else if ( strcasecmp(argv[2], "on") == 0 || strcasecmp(argv[2], "1") == 0 ) ads125xSetPDWN(&ads1256, 1);
+    else {
+        fprintf(stderr, "Invalid PDWN status %s .\n", argv[2]);
+        ads125xClosePDWN(&ads1256);
+        exit(EXIT_FAILURE);
+    }
+    ads125xClosePDWN(&ads1256);
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+    char *env = NULL;
+    if ((env = getenv("ADS1256_DRIVER")) != NULL)
+        if (0 == atoi(env))
+            ADS125xDriverDebug = true;
+
+    if (argc == 1)
+    {
+        fprintf(stderr,
+                "%s: Please provide the arguments.\n"
+                "  Type: ads1256 -h for full details.\n",
+                argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if (strcasecmp(argv[1], "-h") == 0 || strcasecmp(argv[1], "--help") == 0)
+    {
+        fprintf(stdout, "%s: %s\n", argv[0], usage);
+        exit(EXIT_SUCCESS);
+    }
+
+    if (geteuid() != 0)
+    {
+        fprintf(stderr, "%s: Must be root to run. Program should be suid root. This is an error.\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    /**/ if ( strcasecmp (argv[1], "-s") == 0 || strcasecmp (argv[1], "--single"    ) == 0 ) one_shot_read();
+    else if ( strcasecmp (argv[1], "-c") == 0 || strcasecmp (argv[1], "--continuous") == 0 ) doContinuRead(argc, argv);
+    else if ( strcasecmp (argv[1], "-p") == 0 || strcasecmp (argv[1], "--pdwn") == 0)        doPdwn(argc, argv);
+    else if ( strcasecmp (argv[1], "-o") == 0 || strcasecmp (argv[1], "--pdwn") == 0)
+        {fprintf(stderr, "output parameter can only be used with continuous output.\n"); exit(1);}
+    else {
+        fprintf (stderr, "%s: Unknown command: %s.\n", argv [0], argv [1]) ;
+        exit (EXIT_FAILURE) ;
+    }
     return 0;
 }
